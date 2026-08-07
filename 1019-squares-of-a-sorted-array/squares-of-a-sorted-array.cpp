cebc3eed61b953f941fa167aa749c7fee54521ed
chetan-sharma3050 <chsharma2340@gmail.com>
2026-08-07 19:21:42 +0530

    Time: 3 ms (51.93%) | Memory: 31.9 MB (9.59%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,29 +1,72 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> arr(n);
+    vector<int> sortedSquares(vector<int>& a) {
+        int siz=a.size();
+        vector<int> neg;
+        vector<int> pos;
+        
+        for(int i=0;i<siz;i++)
+        {
+            if(a[i]<0)
+            neg.push_back(a[i]);
+            else
+            pos.push_back(a[i]);
 
-        int i = 0;
-        int j = n - 1;
-        int k = n - 1;
+        }
+        if(neg.size()==0) 
+        {
+            for(int i=0;i<pos.size();i++)
+            pos[i]=pos[i]*pos[i];
+            return pos;
+        }
+        if(pos.size()==0) 
+        {
+            for(int i=0;i<neg.size();i++)
+            neg[i]=neg[i]*neg[i];
+            reverse(neg.begin(),neg.end());
+            return neg;
+        }
+       
+        int i=0,j=0;
+        int id=0;
+        int n=neg.size();
+        int m=pos.size();
+        vector<int> res(n+m);
+        for(int i=0;i<n;i++)
+        neg[i]=neg[i]*neg[i];
 
-        while (i <= j) {
-            int A = nums[i] * nums[i];
-            int B = nums[j] * nums[j];
-
-            if (A > B) {
-                arr[k] = A;
+        reverse(neg.begin(),neg.end());
+        for(int i=0;i<m;i++)
+        pos[i]=pos[i]*pos[i];
+        while(i<n and j<m)
+        {
+            if(neg[i]<=pos[j])
+            {
+                res[id]=neg[i];
+                id++;
                 i++;
             }
-            else {
-                arr[k] = B;
-                j--;
+            else
+            {
+                res[id]=pos[j];
+                id++;
+                j++;
             }
-
-            k--;
+        } 
+        while(i<n)
+        {
+            res[id]=neg[i];
+                id++;
+                i++;
+        }
+        while(j<m)
+        {
+            res[id]=pos[j];
+                id++;
+                j++;
         }
 
-        return arr;
+
+        return res;
     }
 };
