3dcad2a0d42df0bb644c6b814824c92fdfdac241
chetan-sharma3050 <chsharma2340@gmail.com>
2026-08-21 08:09:31 +0530

    Time: 56 ms (13.94%) | Memory: 75.6 MB (32.36%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,10 +10,54 @@
  */
 class Solution {
 public:
+  ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode* a=list1;
+        ListNode* b=list2;
+
+        ListNode* temp=new ListNode(100);
+        ListNode* temphead=temp;
+        
+        while(a!=NULL && b!=NULL){
+            if(a->val<=b->val){
+                temp->next=a;
+                a=a->next;
+            }else{
+                temp->next=b;
+                b=b->next;
+            }
+            temp=temp->next;
+        }
+        if(a!=NULL){
+            temp->next=a;
+        }
+        if(b!=NULL){
+            temp->next=b;
+        }
+    
+        return temphead->next;
+        
+    }
+
     ListNode* sortList(ListNode* head) {
 
         if (head == NULL || head->next == NULL)
             return head;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        ListNode*b=slow->next;
+        slow->next=NULL;
+        ListNode*a=head;
+        a=sortList(a);
+        b=sortList(b);
+        ListNode*c=merge(a,b);
+        return c;
+
+
 
         vector<int> arr;
 
