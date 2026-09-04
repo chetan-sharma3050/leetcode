5df48bd61d8b329e11d96d6a1defb07cf747f044
chetan-sharma3050 <chsharma2340@gmail.com>
2026-09-04 08:47:22 +0530

    Time: 3 ms (9.24%) | Memory: 8.8 MB (88.93%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-        for (char ch : s) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            } else {
-                if (st.empty())
-                    return false;
-
-                if ((ch == ')' && st.top() != '(') ||
-                    (ch == '}' && st.top() != '{') ||
-                    (ch == ']' && st.top() != '['))
-                    return false;
-
-                st.pop();
+       stack<char>st;
+       if(s.length()%2!=0) return false;
+       for(int i=0;i<s.length();i++){
+        if(s[i]=='('|| s[i]=='{'|| s[i]=='['){
+            st.push(s[i]);
+        }
+        else if(s[i]==')'){
+            if(st.empty()){
+               return false;
+            }
+            else if(st.top()=='('){
+                st.pop();   
+            }
+            else{
+                return false;
             }
         }
-
-        return st.empty();
+         else if(s[i]==']'){
+            if(st.empty()){
+               return false;
+            }
+            else if(st.top()=='['){
+                st.pop();   
+            }
+            else{
+                return false;
+            }
+        }
+        else if(s[i]=='}'){
+            if(st.empty()){
+               return false;
+            }
+            else if(st.top()=='{'){
+                st.pop();   
+            }
+            else{
+                return false;
+            }
+        }
+       }
+       if(st.empty()) return true;
+       else return false;
     }
 };
